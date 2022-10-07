d52e202ed099d6d475faf2cdcb8f54f5b6f7ac56
Szabi <szabolcs.gyori97@gmail.com>
2022-10-07 09:40:55 +0300

    changed urls

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3000',
-})
+    baseURL: 'https://whispering-thicket-82174.herokuapp.com',
+});
